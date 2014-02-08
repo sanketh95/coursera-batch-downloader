7554b7ad98f98f45af4b1ca558683f1917cccce7
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2014-02-08 10:57:46 +0530

    Coursera Batch Downloader Initial Commit

---
+++ Chrome Extension/myscript.js
@@ -0,0 +1,23 @@
+var jsondata='';
+$('.course-item-list-header').each( function(index, val){
+	var name = $('h3',this).html();
+	var selection_list = $('.course-item-list-section-list:eq('+index+')');
+	var pattern = new RegExp('nbsp;[ \t]*(.*)');
+	var re = pattern.exec(name);
+	var fname = re[re.length-1	];
+
+
+	jsondata = jsondata+'{ "title" : "'+fname+'","links":[';
+	$('li',selection_list).each(function(ind, val){
+		var linkaddr = $('a:eq(2)',$('.course-lecture-item-resource',this)).attr('href');
+		var title=$('a', this).html();
+		title = title.replace(/^\s+|\s+$/g,'')	
+		jsondata = jsondata+'{"title":"'+title+'","link":"'+linkaddr+'"},';
+	});
+	jsondata= jsondata.replace(/(^,)|(,$)/g , '');
+	jsondata+=']},'
+
+});
+
+jsondata= jsondata.replace(/(^,)|(,$)/g , '');
+console.log('{"data":['+jsondata+']}');
